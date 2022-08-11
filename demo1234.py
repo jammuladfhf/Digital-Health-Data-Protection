b12723d6ab7829a166f733ae98f298900f60f423
Narender Jammula <38723314+jammuladfhf@users.noreply.github.com>
2022-08-11 18:17:25 +0530

    req edit

--- demo1234.py
+++ demo1234.py
@@ -10,28 +10,39 @@ import sqlite3
 conn = sqlite3.connect('demo.db')
 cur = conn.cursor()
 
-def form():
-    st.write("This is a form")
-    with st.form(key='Information form'):
-        name=st.text_input("Enter your name: ")
-        age=st.text_input("Enter age:")
-        submission = st.form_submit_button(label='Submit')
-        if submission == True:
-            addData(name, age)
-        printdata()
-            
-def addData(name,age):
-    cur.execute(""" Create table if not exists clg_form(Name Text(50), Age Text(50));""")
-    cur.execute("insert into clg_form values (?, ?)",(name, age))
-    conn.commit()
-    conn.close()
-    st.success('Successfully Submited')
+import uuid
+DIGITS = 8  # number of hex digits of the UUID to use
+print(uuid.uuid4().hex[:DIGITS] )  #{cid: int(uuid.uuid4().hex[:DIGITS], base=16) for cid in original_ids}
 
-def printdata():
-    conn1 = sqlite3.connect('demo.db')
-    cur1 = conn1.cursor()
-    df = pd.read_sql(" select * from clg_form ", con=conn1)
-    st.write(df)
-    
-    
-form()
+
+
+
+
+
+# =============================================================================
+# def form():
+#     st.write("This is a form")
+#     with st.form(key='Information form'):
+#         name=st.text_input("Enter your name: ")
+#         age=st.text_input("Enter age:")
+#         submission = st.form_submit_button(label='Submit')
+#         if submission == True:
+#             addData(name, age)
+#         printdata()
+#             
+# def addData(name,age):
+#     cur.execute(""" Create table if not exists clg_form(Name Text(50), Age Text(50));""")
+#     cur.execute("insert into clg_form values (?, ?)",(name, age))
+#     conn.commit()
+#     conn.close()
+#     st.success('Successfully Submited')
+# 
+# def printdata():
+#     conn1 = sqlite3.connect('demo.db')
+#     cur1 = conn1.cursor()
+#     df = pd.read_sql(" select * from clg_form ", con=conn1)
+#     st.write(df)
+#     
+#     
+# form()
+# =============================================================================
